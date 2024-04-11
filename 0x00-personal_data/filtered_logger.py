3d8be2ea16e7a389f6d4ed45ff06b765ec894a9e
okeomasilchi <okeomasilachi@gmail.com>
2024-04-11 14:09:59 +0100

    Update get_db function to include return type annotation in filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -65,7 +65,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Retrieve database credentials from
     environment variables

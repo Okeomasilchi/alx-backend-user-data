b9890b178d4905cf8540e20e40c4c922e2d9a7aa
okeomasilchi <okeomasilachi@gmail.com>
2024-04-11 14:44:02 +0100

    Add hash_password function to encrypt_password.py and update main.py to use it

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,3 +3,23 @@
 """
 Model for Encrypt Password
 """
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+  """
+  Hashes the given password using bcrypt.
+
+  Args:
+    password (str): The password to be hashed.
+
+  Returns:
+    bytes: The hashed password.
+
+  """
+  salt = bcrypt.gensalt()  # Generate a random salt
+  # Hash the password with the salt
+  hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+  return hashed_password
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 """
-Model for Logging
+This module provides functionality for logging and filtering user data.
+
+It includes functions for obfuscating sensitive information in log
+messages, a custom formatter for redacting PII (Personally Identifiable
+Information) fields, and a logger object configured to log user data
+with redacted PII fields.
+
+The main function retrieves user data from a database and logs it using
+the logger.
+
+Author: Okeomasilachi
 """
 
 
@@ -74,7 +84,7 @@ def get_db() -> MySQLConnection:
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
 
     try:  # try Connecting to the database
         connection = mysql.connector.connect(
@@ -86,3 +96,26 @@ def get_db() -> MySQLConnection:
         return connection if connection.is_connected() else None
     except mysql.connector.Error as err:
         print("Error:", err)
+
+
+def main() -> None:
+    """
+    Entry point of the program.
+
+    Retrieves user data from the database and logs it using the logger.
+    """
+    logger = get_logger()
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    param = "name={}; email={}; phone={}; ssn={}; " + \
+        "password={}; ip={}; last_login={}; user_agent={};"
+    for row in cursor.fetchall():
+        logger.info(
+            param.format(*row))
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

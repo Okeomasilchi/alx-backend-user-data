10ebc800079151618afdacfcebb28e990a276131
okeomasilchi <okeomasilachi@gmail.com>
2024-04-11 14:45:00 +0100

    Refactor hash_password function to include docstring in encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,17 +9,17 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-  """
-  Hashes the given password using bcrypt.
+    """
+    Hashes the given password using bcrypt.
 
-  Args:
-    password (str): The password to be hashed.
+    Args:
+      password (str): The password to be hashed.
 
-  Returns:
-    bytes: The hashed password.
+    Returns:
+      bytes: The hashed password.
 
-  """
-  salt = bcrypt.gensalt()  # Generate a random salt
-  # Hash the password with the salt
-  hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-  return hashed_password
+    """
+    salt = bcrypt.gensalt()  # Generate a random salt
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password

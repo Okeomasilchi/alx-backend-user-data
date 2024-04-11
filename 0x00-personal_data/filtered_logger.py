5988d364891952964ec9597ea64726318af49ad6
okeomasilchi <okeomasilachi@gmail.com>
2024-04-11 12:40:41 +0100

    Refactor filter_datum function to improve readability and performance in filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,14 @@ Model for Logging
 import re
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def check(fie: list, red: str, mess: str, sep: str) -> str:
+    """Handles obfuscation of log message"""
+    pattern = r"(" + "|".join(fie) + r")=.*?(?=" + \
+        re.escape(sep) + r"|$)"
+    return re.sub(pattern, r"\1=" + red, mess)
+
+
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
-    return re.sub(
-      r"(" + "|".join(fields) + r")=.*?(?=" + re.escape(separator) + r"|$)",
-                  r"\1=" + redaction, message)
+    return check(fields, redaction, message, separator)

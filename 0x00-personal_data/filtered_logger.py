257800fd1d53c91bc155fa183f29b4be5055489b
okeomasilchi <okeomasilachi@gmail.com>
2024-04-11 12:20:41 +0100

    Add filter_datum function to filter sensitive data in filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,3 +3,28 @@
 """
 Model for Logging
 """
+
+
+import re
+
+
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Filter sensitive data from a message.
+
+    Args:
+      fields (list): A list of strings representing
+        the sensitive fields to be filtered.
+      redaction (str): The string to replace the sensitive
+        data with.
+      message (str): The message containing the sensitive data.
+      separator (str): The separator used to separate the
+        fields in the message.
+
+    Returns:
+      str: The filtered message with the sensitive data replaced.
+    """
+    regex = r"(" + "|".join(fields) + r")=.*?(?=" + \
+        re.escape(separator) + r"|$)"
+    return re.sub(regex, r"\1=" + redaction, message)

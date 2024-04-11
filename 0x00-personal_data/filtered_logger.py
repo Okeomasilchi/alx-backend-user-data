de25e431090050c0d4daf4d03c72d7031d81c44e
okeomasilchi <okeomasilachi@gmail.com>
2024-04-11 12:32:45 +0100

    Refactor filter_datum function in filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,23 +8,8 @@ Model for Logging
 import re
 
 
-def filter_datum(fields: list, redaction: str,
-                 message: str, separator: str) -> str:
-    """
-    Filter sensitive data from a message.
-
-    Args:
-      fields (list): A list of strings representing
-        the sensitive fields to be filtered.
-      redaction (str): The string to replace the sensitive
-        data with.
-      message (str): The message containing the sensitive data.
-      separator (str): The separator used to separate the
-        fields in the message.
-
-    Returns:
-      str: The filtered message with the sensitive data replaced.
-    """
-    regex = r"(" + "|".join(fields) + r")=.*?(?=" + \
-        re.escape(separator) + r"|$)"
-    return re.sub(regex, r"\1=" + redaction, message)
+def filter_datum(fields, redaction, message, separator) -> str:
+    """returns the log message obfuscated"""
+    return re.sub(
+      r"(" + "|".join(fields) + r")=.*?(?=" + re.escape(separator) + r"|$)",
+                  r"\1=" + redaction, message)

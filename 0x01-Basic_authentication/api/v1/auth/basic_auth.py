e643d391ebf1875162781117c99cf92b1322a644
okeomasilchi <okeomasilachi@gmail.com>
2024-04-15 22:44:15 +0100

    Refactor authentication logic and add support for extracting base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class to manage the API authentication"""
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        if authorization_header is None or not isinstance(
+            authorization_header,
+            str
+        ):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]

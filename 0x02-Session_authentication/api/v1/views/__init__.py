3fedfb8f1ef6375f74f7c78c7b597d4eab478ce3
okeomasilchi <okeomasilachi@gmail.com>
2024-04-18 11:00:05 +0100

    Refactor API views module to improve readability and maintainability

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
 """
+This module contains the blueprint for the API views.
+
+It defines the `app_views` blueprint object, which is used
+to group related API endpoints. The blueprint is registered
+with the Flask application and has a URL prefix of '/api/v1'.
+
+The module also imports the necessary views for the API
+endpoints and loads user data from a file.
+"""
+
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
-
+from api.v1.views.session_auth import *
 User.load_from_file()
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Module of Users views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+import os
+
+
+@app_views.route(
+    '/auth_session/login',
+    methods=['POST'],
+    strict_slashes=False
+)
+def login():
+    """
+    Logs in a user by validating the email and password
+    provided in the request form.
+
+    Returns:
+        A JSON response containing the user information if
+        the login is successful. Otherwise, returns an error
+        JSON response with the appropriate status code.
+    """
+    email = request.form.get('email', None)
+    passwd = request.form.get('password', None)
+    if not email or not email.strip():
+        return jsonify({"error": "email missing"}), 400
+    elif not passwd or not passwd.strip():
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": email})
+
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    elif not user[0].is_valid_password(passwd):
+        return jsonify({"error": "wrong password"}), 401
+    else:
+        from api.v1.app import auth
+        user = user[0]
+        session_id = auth.create_session(user.id)
+
+        response = jsonify(user.to_json())
+        response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+        return response

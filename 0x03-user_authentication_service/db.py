5aadb0ba6976ebc85bfef8c11d819047167be51b
okeomasilchi <okeomasilachi@gmail.com>
2024-04-24 15:52:09 +0100

    Refactor find_user_by method in DB class to handle InvalidRequestError

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -83,7 +83,10 @@ class DB:
           NoResultFound: If no user is found that matches
           the given criteria.
         """
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
-            raise NoResultFound()
-        return user
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound()
+            return user
+        except InvalidRequestError:
+            raise InvalidRequestError()

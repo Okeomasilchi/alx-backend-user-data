34e99e17d569aaf6f1b9c442c71f72c3da989505
okeomasilchi <okeomasilachi@gmail.com>
2024-04-24 14:52:58 +0100

    Add user authentication functionality to DB module

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,1 +1,65 @@
 #!/usr/bin/env python3
+
+"""
+DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import User
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str = None,
+                 hashed_password: str = None) -> object:
+        """
+        Adds a new user to the database.
+
+        Args:
+          email (str): The email of the user.
+          hashed_password (str): The hashed password of the user.
+
+        Returns:
+          object: The newly created user object.
+
+        Raises:
+          ValueError: If either email or hashed_password is not
+            provided.
+          ValueError: If a user with the same email already
+            exists in the database.
+        """
+        if email is None:
+            raise ValueError("Email is required")
+        if hashed_password is None:
+            raise ValueError("hashed_password is required")
+        user = User(email=email, hashed_password=hashed_password)
+        try:
+            self._session.add(user)
+            self._session.commit()
+        except IntegrityError as e:
+            self._session.rollback()
+            raise ValueError("User with this email already exists")
+        return user

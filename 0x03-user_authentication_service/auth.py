4bdc34429475415118b6d2cfd4c8563d3a0f8df6
okeomasilchi <okeomasilachi@gmail.com>
2024-04-25 05:32:10 +0100

    Refactor registration process in Auth class to include email validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,8 @@ Auth model for all authentication mechanisms
 
 
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -22,3 +24,35 @@ def _hash_password(password: str) -> bytes:
         password.encode('utf-8'),
         salt
     )
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of the Auth class.
+
+        The __init__ method is called when a new object of the
+        Auth class is created. It initializes the instance
+        variable _db with an instance of the DB class.
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """
+        Registers a user in the database.
+
+        Args:
+          email (str): The email address of the user.
+          password (str): The password of the user.
+
+        Returns:
+          None
+        """
+        if self._db._session.query(User).filter_by(email=email).first():
+            raise ValueError(f"User '{email}' already exists")
+
+        return self._db.add_user(email, _hash_password(password))

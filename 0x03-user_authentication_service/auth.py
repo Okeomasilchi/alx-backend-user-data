d8a474e92b50624a126bc200781af22706470831
okeomasilchi <okeomasilachi@gmail.com>
2024-04-25 05:04:29 +0100

    Refactor password hashing function to use bcrypt in auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""
+Auth model for all authentication mechanisms
+"""
+
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+        Hashes the given password using bcrypt.
+
+        Args:
+          password (str): The password to be hashed.
+
+        Returns:
+          bytes: The hashed password.
+        """
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(
+        password.encode('utf-8'),
+        salt
+    )

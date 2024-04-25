dca1cd4c02b6c26c9a4ef44f7da3857e8fd9ee43
okeomasilchi <okeomasilachi@gmail.com>
2024-04-25 05:52:54 +0100

    Refactor app.py to include user registration route in the Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,15 +4,29 @@ Basic Flask app
 """
 
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def welcome():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": new_user.email,
+                        "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
